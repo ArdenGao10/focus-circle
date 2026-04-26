ba828e44dbce5f75c4bf0e2cf2ea2cf0c861dffd
Eternalchu <3214175397@qq.com>
2026-04-26 17:18:05 +0800

    chore: 同步部署 Vercel

--- next.config.ts
+++ next.config.ts
@@ -6,5 +6,8 @@ const nextConfig: NextConfig = {
 
 export default nextConfig;
 
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
+if (process.env.NODE_ENV === "development") {
+  import("@opennextjs/cloudflare").then(({ initOpenNextCloudflareForDev }) =>
+    initOpenNextCloudflareForDev()
+  );
+}
